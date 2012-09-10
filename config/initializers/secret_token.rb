ddaa2d4b7b6ef825ef1c62350d3906f5ba4c65b5
Kashif.Umair <kashif.umair@7vals.com>
2012-09-10 16:26:49 +0500

    First commit for my application.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FbBirthdays::Application.config.secret_token = '811928874c2851d3d42bbabadba349c2c35686b269779c2b14a36fec46e0960c0b2494b7e3172bd9514b6a6d2ddc3356713c0d958b2d829e480a69740d5d5349'
